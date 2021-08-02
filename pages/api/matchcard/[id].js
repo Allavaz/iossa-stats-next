4560f446db5bb6f7b4a67eb53a89ec4f777725f2
David Zavalla <davidzavalla@gmail.com>
2021-08-02 20:22:49 -0300

    get match card height from the div

--- pages/api/matchcard/[id].js
+++ pages/api/matchcard/[id].js
@@ -5,18 +5,26 @@ export default async function handler(req, res) {
   if (req.method === 'GET') {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    let {template, height} = await MatchCardTemplate(req.query.id);
+    let template = await MatchCardTemplate(req.query.id);
     await page.setViewport({
       width: 670,
-      height: height
+      height: 480
     })
     await page.setContent(template,
       {waitUntil: 'networkidle0'}
     );
     await page.addStyleTag({path: './styles/matchCardStyle.css'});
-    let image = await page.screenshot();
+    const container = await page.$('.container');
+    const boundingBox = await container.boundingBox();
+    let image = await page.screenshot({
+      clip: {
+        x: 0,
+        y: 0,
+        width: 670,
+        height: Math.round(boundingBox.height)
+      }
+    });
     res.setHeader('Content-Type', 'image/png');
     res.send(image);
-    await browser.close();
   }
 }
