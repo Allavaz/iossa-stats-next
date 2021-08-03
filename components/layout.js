ae29d9adc1621414d69f583fb60d5a41ba7d946d
David Zavalla <davidzavalla@gmail.com>
2021-08-03 11:54:07 -0300

    fix bug where fontawesomeicons flash in giant size

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,13 @@
-import Header from "./header"
-import Footer from "./footer"
-import Navigation from "./navigation"
+import Header from "./header";
+import Footer from "./footer";
+import Navigation from "./navigation";
+
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
 
 export default function Layout({ children }) {
   return (
