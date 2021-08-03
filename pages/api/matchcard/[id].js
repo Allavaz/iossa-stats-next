8cf23c03d24838857284af8d316fd8bf499227eb
David Zavalla <davidzavalla@gmail.com>
2021-08-03 13:31:38 -0300

    cache matchcards so the babaneta doesnt suffer too much

--- pages/api/matchcard/[id].js
+++ pages/api/matchcard/[id].js
@@ -1,31 +1,16 @@
-import puppeteer from "puppeteer";
-import MatchCardTemplate from "../../../lib/matchCardTemplate";
+import fs from 'fs';
+import createMatchCard from "../../../lib/createMatchCard";
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    let template = await MatchCardTemplate(req.query.id);
-    await page.setViewport({
-      width: 670,
-      height: 480
-    })
-    await page.setContent(template,
-      {waitUntil: 'networkidle0'}
-    );
-    await page.addStyleTag({path: './styles/matchCardStyle.css'});
-    const container = await page.$('.container');
-    const boundingBox = await container.boundingBox();
-    let image = await page.screenshot({
-      clip: {
-        x: 0,
-        y: 0,
-        width: 670,
-        height: Math.round(boundingBox.height)
-      }
-    });
+    let dir = `./.cache/matchcards/${process.env.DB_COLLECTION}/${req.query.id}.png`;
+    let image;
+    if (fs.existsSync(dir)) {
+      image = fs.readFileSync(dir);
+    } else {
+      image = await createMatchCard(req.query.id);
+    }
     res.setHeader('Content-Type', 'image/png');
     res.send(image);
-    await browser.close();
   }
 }
